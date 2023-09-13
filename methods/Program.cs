ffcdca1a18cfdd727d8b1585a48311f856ee5f36
mthem <mthem@INDLEYETHU>
2023-09-13 22:31:54 +0200

    Add project files.

---
+++ methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sayhi();
+            Addnumbers
+            Console.ReadKey();
+        }
+
+        static void sayhi() {
+
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+
+        
+        }
+
+        static int Addnumbers();
+
+        Console.WriteLine("enter first number");
+           
+        int num1 = Convert.ToInt32(Console.ReadLine()); 
+
+
+
+
+
+
+    }
+}
